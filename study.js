4c976936b7e6dce6d7535e2181eb3d88d382c1e1
rshday <rshday@gmail.com>
2020-08-12 09:00:48 +0900

    this commit is logo button create!

--- study.js
+++ study.js
@@ -1,7 +1,19 @@
 var str = "text";
 
+function logo_click() {
+  document.getElementById('logo').innerHTML = "PAPAO";
+  window.location.href = window.location.href;
+}
+
+
 function button1_click(){
-  str = "click";
-  alert(str);
-  action.reload();
+  document.getElementById('contens').innerHTML
+  ="안녕하세요. 파파오입니다.<br/>"
++"파파오 서비스를 이용해 주시는 회원 여러분께 감사드리며, 운영정책 변경에 관해 안내 말씀 드립니다</br>"
+}
+function button1_click2() {
+  document.getElementById('contens').innerHTML
+  ="안녕하세요.<br/>"
+  +"나의 소개를 합니다.<br/>"
+  +"감사합니다.";
 }
