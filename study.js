e0e3d85993babe0e0217b8069c46471f7e4501fd
rshday <rshday@gmail.com>
2020-08-17 00:27:25 +0900

    header js code test

--- study.js
+++ study.js
@@ -1,4 +1,8 @@
 'use strict'
+function init(){
+  document.querySelector(".header").innerHTML="Hello!"
+}
+init();
 
 const btn = document.querySelector(".btn");
 btn.addEventListener("click",btnclick);
@@ -9,11 +13,13 @@ function btnclick() {
     btn.innerHTML=`
     btn
     <div id="box">
-    이런건 어떡하면 만들수 있지?
+    이런건 어떡하면 만들수 있지???
     </div>
     `
   }
   else{
     btn.innerHTML="btn";
   }
+  
+  
 }
